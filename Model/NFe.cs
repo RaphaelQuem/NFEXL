106f0577feeb19a679d91ccdb639c259edd8d714
Raphael Maia <raphzmaia@gmail.com>
2017-07-07 10:53:33 -0300

    IDEvent implemented

--- Model/NFe.cs
+++ Model/NFe.cs
@@ -13,90 +13,35 @@ namespace NFEXL.Model
     public class NFe : IFiscalDocument
     {
         [XL(Name = "DATA", Order = 1)]
-        public string EmissionDate
-        {
-            get;
-            set;
-        }
+        public string EmissionDate { get; set; }
         [XL(Name = "CHAVE", Order = 2)]
-        public string Key
-        {
-            get;
-            set;
-        }
+        public string Key { get; set; }
         [XL(Name = "NUMERO", Order = 3)]
-        public uint DocumentNumber
-        {
-            get;
-            set;
-        }
+        public uint DocumentNumber { get; set; }
         [XL(Name = "MOD", Order = 4)]
-        public string Mod
-        {
-            get;
-            set;
-        }
+        public string Mod { get; set; }
         [XL(Name = "TIPO", Order = 5)]
-        public int Type
-        {
-            get;
-            set;
-        }
+        public int Type { get; set; }
         [XL(Name = "UF", Order = 6)]
-        public string State
-        {
-            get;
-            set;
-        }
+        public string State { get; set; }
         [XL(Name = "CPF", Order = 7)]
-        public string ClientCode
-        {
-            get;
-            set;
-        }
+        public string ClientCode { get; set; }
         [XL(Name = "NOME CLI", Order = 8)]
-        public string ClientName
-        {
-            get;
-            set;
-        }
+        public string ClientName { get; set; }
         [XL(Name = "UF CLI", Order = 9)]
-        public string ClientState
-        {
-            get;
-            set;
-        }
+        public string ClientState { get; set; }
         [XL(Name = "CNPJ", Order = 10)]
-        public string CompanyCode
-        {
-            get;
-            set;
-        }
+        public string CompanyCode { get; set; }
         [XL(Name = "RAZAO", Order = 11)]
         public string CompanyName { get; set; }
         [XL(Name = "ITEMS", Order = 99)]
-        public List<IFiscalDocumentItem> Items
-        {
-            get;
-            set;
-        }
+        public List<IFiscalDocumentItem> Items { get; set; }
         [XL(Name = "DESCONTO", Order = 99)]
-        public double TotalDiscount
-        {
-            get;
-            set;
-        }
+        public double TotalDiscount { get; set; }
         [XL(Name = "FRETE", Order = 99)]
-        public double TotalShipping
-        {
-            get;
-            set;
-        }
+        public double TotalShipping { get; set; }
         [XL(Name = "TOTAL", Order = 99)]
-        public double TotalValue
-        {
-            get; set;
-        }
+        public double TotalValue { get; set; }
 
         public NFe(XmlDocument doc)
         {
